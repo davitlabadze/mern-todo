77ad0239e900714c066af7b67c29d4ebf6a71a1e
davitlabadze <labadzedato18@gmail.com>
2022-05-20 00:01:58 +0400

    feat: add action

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import { DotsHorizontalIcon } from '@heroicons/react/solid';
+import TaskAction from './components/TaskAction';
 function App() {
   return (
-    <div className='flex h-screen bg-pink-600'>
+    <div className='flex h-screen bg-pink-500'>
       <div className='container w-1/2 h-auto mx-auto mt-20 mb-20 text-center bg-gray-700 rounded'>
         <input
           className='w-1/2 p-4 px-10 mt-10 bg-gray-800 bg-no-repeat border border-gray-900 rounded shadow-xl outline-none text-slate-100 bg-left-1 bg bg-task'
@@ -11,8 +11,20 @@ function App() {
         />
         <div className='container w-1/2 h-auto mx-auto mt-10 text-left bg-gray-700 rounded'>
           <div className='flex justify-between w-full p-4 text-gray-400 border border-transparent rounded hover:border-gray-600 hover:border h-14'>
-            <h1>test</h1>
-            <DotsHorizontalIcon className='w-6 h-6 cursor-pointer' />
+            <h1>Eat</h1>
+            <TaskAction />
+          </div>
+          <div className='flex justify-between w-full p-4 text-gray-400 border border-transparent rounded hover:border-gray-600 hover:border h-14'>
+            <h1>Sleep</h1>
+            <TaskAction />
+          </div>
+          <div className='flex justify-between w-full p-4 text-gray-400 border border-transparent rounded hover:border-gray-600 hover:border h-14'>
+            <h1>Code</h1>
+            <TaskAction />
+          </div>
+          <div className='flex justify-between w-full p-4 text-gray-400 border border-transparent rounded hover:border-gray-600 hover:border h-14'>
+            <h1>Repeat</h1>
+            <TaskAction />
           </div>
         </div>
       </div>
---
+++ frontend/src/components/TaskAction.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import {
+  DotsHorizontalIcon,
+  PencilIcon,
+  TrashIcon,
+} from '@heroicons/react/outline';
+
+function TaskAction() {
+  const [isOpen, toggleOpen] = useState(false);
+  const toggle = () => {
+    toggleOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button onClick={toggle}>
+        <DotsHorizontalIcon className='w-6 h-6 cursor-pointer' />
+      </button>
+      {isOpen ? (
+        <ul className='absolute h-auto mt-5 -ml-12 text-gray-500 bg-gray-600 rounded-lg shadow-lg '>
+          <li
+            className='flex p-2 mt-2 mb-2 hover:text-gray-300'
+            onClick={() => toggleOpen(false)}
+          >
+            <PencilIcon className='w-6 h-6 ' />
+            <span className='ml-2 cursor-pointer text-md'>Edit</span>
+          </li>
+          <li
+            className='flex p-2 mt-2 mb-2 hover:text-gray-300'
+            onClick={() => toggleOpen(false)}
+          >
+            <TrashIcon className='w-6 h-6 ' />
+            <span className='ml-2 cursor-pointer text-md '>Delete</span>
+          </li>
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+export default TaskAction;
