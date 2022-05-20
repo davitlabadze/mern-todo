205fa8c34ebd66da7988db657bc95363dd441208
davitlabadze <labadzedato18@gmail.com>
2022-05-20 20:10:01 +0400

    feat: set todo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [todos, setTodos] = useState([]);
+  const [text, setText] = useState('');
 
   useEffect(() => {
     getTodos();
@@ -28,24 +29,48 @@ function App() {
     }
   };
 
+  const setTodo = async (e) => {
+    e.preventDefault();
+    await axios
+      .post('todos', {
+        text: text,
+      })
+      .then(() => {
+        getTodos();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className='flex h-screen bg-pink-500'>
       <div className='container w-1/2 h-auto mx-auto mt-20 mb-20 text-center bg-gray-700 rounded'>
-        <input
-          className='w-1/2 p-4 px-10 mt-10 bg-gray-800 bg-no-repeat border border-gray-900 rounded shadow-xl outline-none text-slate-100 bg-left-1 bg bg-task'
-          type='text'
-          name='text'
-          placeholder='Add a task...'
-        />
+        <form onSubmit={setTodo}>
+          <input
+            className='w-1/2 p-4 px-10 mt-10 bg-gray-800 bg-no-repeat border border-gray-900 rounded shadow-xl outline-none text-slate-100 bg-left-1 bg bg-task'
+            type='text'
+            name='text'
+            id='text'
+            required
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </form>
         <div className='container w-1/2 h-auto mx-auto mt-10 text-left bg-gray-700 rounded'>
           {!isLoading &&
             todos.length > 0 &&
             todos.map((todo) => (
-              <div className='flex justify-between w-full p-4 text-gray-400 border border-transparent rounded hover:border-gray-600 hover:border h-14'>
-                <h1>{todo.text}`</h1>
+              <div
+                key={todo._id}
+                className='flex justify-between w-full p-4 text-gray-400 border border-transparent rounded hover:border-gray-600 hover:border h-14'
+              >
+                <h1>{todo.text}</h1>
                 <TaskAction />
               </div>
             ))}
+          {!isLoading && todos.length === 0 && <h1>not yet todos</h1>}
+          {isLoading && <h1>loading</h1>}
         </div>
       </div>
     </div>
